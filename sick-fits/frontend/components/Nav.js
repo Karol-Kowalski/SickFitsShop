07626acbcee7aa5a6d197fa808e302c8af78196a
Virion <kajko195@tlen.pl>
2021-02-14 00:05:32 +0100

    Modified frontend app

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <nav>
-      <Link href="product">product</Link>
+      <Link href="products">products</Link>
       <Link href="sell">sell</Link>
       <Link href="orders">orders</Link>
       <Link href="account">account</Link>
