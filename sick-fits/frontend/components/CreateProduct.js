065d0dfd772a4e39f57db3e102a10e99c04f47b8
Virion <kajko195@tlen.pl>
2021-02-17 12:54:07 +0100

    build custom useForm hook and form on page
    Declare clear and reset form hook and add it to form in CreateProduct
    component.
    Declare condition in handleChange (useForm hook) that parse string
    recived from input to number. GraphQL server expect price as number not
    string.

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,7 +1,7 @@
 import useForm from '../lib/useForm';
 
 export default function CreateProduct() {
-  const { inputs, handleChange} = useForm({
+  const { inputs, handleChange, resetForm, clearForm } = useForm({
     name: 'Nice Shoes',
     price: 3456,
     description: 'Wow!!!',
@@ -20,6 +20,24 @@ export default function CreateProduct() {
           onChange={handleChange}
         />
       </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Clear Form
+      </button>
     </form>
   );
 }
