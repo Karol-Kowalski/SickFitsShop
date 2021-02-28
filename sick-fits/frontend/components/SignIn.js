3d77862f76404e32d40100e387c08dbdd1f45956
Virion <kajko195@tlen.pl>
2021-02-28 21:53:08 +0100

    Add Sing In component
    now user can get authorisation if is registered database. in other wise
    doesn't see components for authorised users.

---
+++ sick-fits/frontend/components/SignIn.js
@@ -0,0 +1,80 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import { CURRENT_USER_QUERY } from './User';
+import DisplayError from './ErrorMessage';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    AuthenticateWithPassword(email: $email, password: $password) {
+      ... on UserAuthenticationWithPasswordSuccess {
+        item {
+          id
+          email
+          name
+        }
+      }
+      ... on UserAuthenticationWithPasswordFailure {
+        code
+        message
+      }
+    }
+  }
+`;
+
+export default function SignIn() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+  });
+
+  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+    // refetch the currently loggedin user
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+  async function handleSubmit(e) {
+    e.preventDfault();
+    await signin();
+    resetForm();
+  }
+  const error =
+    data?.authenticateWithPassword.__typename ===
+    'UserAuthenticationWithPasswordFailure'
+      ? data?.authenticateWithPassword
+      : undefined;
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Into Your Account</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In!</button>
+      </fieldset>
+    </Form>
+  );
+}
