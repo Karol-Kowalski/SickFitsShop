416d22cf1d572245983535d41436139583e603a8
Virion <kajko195@tlen.pl>
2021-03-07 22:15:25 +0100

    Add AddToCart and RemoveFromCart button
    AddToCart provide adding items to user cart. After adding item Cart will
    open imidiately.
    
    User can delete item from Cart.

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -6,6 +6,7 @@ import { useUser } from './User';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import { useCart } from '../lib/CartState';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
   padding: 0;
@@ -39,6 +40,7 @@ function CartItem({ cartItem }) {
           </em>
         </p>
       </div>
+      <RemoveFromCart id={product.id} />
     </CartItemStyles>
   );
 }
