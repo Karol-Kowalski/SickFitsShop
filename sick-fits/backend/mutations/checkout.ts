82176b1c8f796ca8f7d5e9ed764c851ed204c246
Virion <kajko195@tlen.pl>
2021-03-14 21:26:46 +0100

    add custom query checkout

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -1,3 +1,4 @@
+import { CartItem } from '../schemas/CartItem';
 import {
   CartItemCreateInput,
   OrderCreateInput,
@@ -58,7 +59,31 @@ async function checkout(
     throw new Error(err.message);
   })
   // 4. Convert cartItems to OrderItems
+  const orderItems = cartItems.map(cartItem => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+      user: { connect: { id: userId } },
+    }
+    return orderItem;
+  })
   // 5. Create order and return it
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+    }
+  })
+  // 6. Clean up any old cart item
+  const cartItemIds = user.cart.map(cartItem => cartItem.id)
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds,
+  })
+  return order;
 }
 
 export default checkout;
